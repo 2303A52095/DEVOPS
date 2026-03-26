01e618b8500a455d779d406a585662fe6a802594
Ramcharan <2303A52095@sru.edu.in>
2026-03-26 09:37:26 +0530

    Delete routes directory

--- routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { renderDashboard } = require('../controllers/dashboardController');
-const { isAuthenticated } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get('/', isAuthenticated, renderDashboard);
-
-module.exports = router;
